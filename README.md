82c9933abf50f05383aa0fbddbcc2177dd38f058
ForCoderjwu <jwu85@dons.usfca.edu>
2021-02-28 15:42:28 +0800

    Initial the file

---
+++ README.md
@@ -0,0 +1,13 @@
+## Project 1 - Simple student's hand book
+#### by Jiaquan Wu
+#### 2/28/2021
+
+
+### Overview
+This program make up a student's hand book for their parents to used. It used the non-linear designing
+and clickable buttom.
+
+
+### Technical Details
+
+Modify the sketch.js setup() and draw() functions and begin adding functions!
---
+++ sketch.js
@@ -0,0 +1,20 @@
+/***********************************************************************************
+  Project 1 - Simple student's hand book
+  by Jiaquan Wu
+  
+  -------------------------------------------------------------------------------
+  DESCRIPTION:
+    This program make up a student's hand book for their parents to used. It used the non-linear designing
+    and clickable buttom.
+***********************************************************************************/
+
+// Setup code goes here
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(0);
+}
